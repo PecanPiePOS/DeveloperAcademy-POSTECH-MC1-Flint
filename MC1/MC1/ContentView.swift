5885a3e992dbb735924b612c6131aceac68726a8
EuniceNam <caellime@gmail.com>
2022-04-05 12:49:23 +0900

    프로젝트 추가 시도합니다

---
+++ MC1/MC1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MC1
+//
+//  Created by Hyorim Nam on 2022/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Flint!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MC1/MC1/MC1App.swift
@@ -0,0 +1,17 @@
+//
+//  MC1App.swift
+//  MC1
+//
+//  Created by Hyorim Nam on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct MC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
