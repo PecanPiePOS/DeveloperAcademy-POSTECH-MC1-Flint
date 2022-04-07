8a4fbc311168b6babc8616ff99bcf6b900591ec5
EuniceNam <caellime@gmail.com>
2022-04-07 14:27:10 +0900

    Add: OrderCheckView.swift
    View 폴더에 OrderCheckView.swift 파일 추가

---
+++ MC1/MC1/View/OrderCheckView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderCheckView.swift
+//  MC1
+//
+
+import SwiftUI
+
+struct OrderCheckView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderCheckView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderCheckView()
+    }
+}
