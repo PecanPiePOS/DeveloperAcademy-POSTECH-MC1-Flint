f7e69e74ce57dbcc8684b966f88d0147afc1b079
0w0-git <yw3543@gmail.com>
2022-04-07 22:09:09 +0900

    Feat: MenuQuantityView, MenuSetChoiceView 추가

---
+++ MC1/MC1/View/MenuSetChoiceView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuSetChoiceView.swift
+//  MC1
+//
+//  Created by Yeongwoo Kim on 2022/04/07.
+//
+
+import SwiftUI
+
+struct MenuSetChoiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuSetChoiceView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuSetChoiceView()
+    }
+}
