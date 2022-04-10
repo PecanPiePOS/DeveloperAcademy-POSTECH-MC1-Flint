341a5d2718e0afe4d962a62506d0937f9048831d
Youth <99013115+kimscastle@users.noreply.github.com>
2022-04-10 18:16:31 +0900

    Add: BeforeGuideStartView File

---
+++ MC1/MC1/View/BeforeGuideStartView.swift
@@ -0,0 +1,20 @@
+//
+//  BeforeGuideStartView.swift
+//  MC1
+//
+//  Created by uiskim on 2022/04/10.
+//
+
+import SwiftUI
+
+struct BeforeGuideStartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BeforeGuideStartView_Previews: PreviewProvider {
+    static var previews: some View {
+        BeforeGuideStartView()
+    }
+}
